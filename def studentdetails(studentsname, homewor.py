521f444b4e16e202b143ef38f3036da9d858af2c
Jasmin <jwillis2@qa.com>
2021-09-07 15:12:25 +0100

    more stuff

---
+++ def studentdetails(studentsname, homewor.py
@@ -0,0 +1,23 @@
+def studentdetails(studentsname, homeworkscore, assessmentscore, finalexamscore):
+    total = homework + assessment + exam
+    percentage = (total/175) * 100
+    return percentage
+
+name = input("Enter student name: ")
+
+homework = int(input("Enter homework score:"))
+if homework > 25:
+    print("Invalid input")
+    exit()
+assessment = int(input("Enter assessment score: "))
+if assessment > 50:
+    print("Invalid input")
+    exit()
+exam = int(input("Enter Exam result: "))
+if exam > 100:
+    print("Invalid input")
+    exit()
+
+percentresult = studentdetails(name, homework, assessment, exam)
+
+print(name + " achieved a total percentage of: " + str(percentresult) + "%")
